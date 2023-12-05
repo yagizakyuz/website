ca54ed10a5148a4ce4ab6ca5ac47b8b4085da2c5
yagizakyuz <yagizakyuz@users.noreply.github.com>
2023-12-05 14:17:06 +0000

    deploy: aabeeabb9391af0f3da77d1838a1a972fff20238

---
+++ src/routes/+page.svelte
@@ -0,0 +1,7 @@
+<h1>Welcome to SvelteKit</h1>
+<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<button class="btn btn-primary">Click me</button>
+
+<script>
+    import '../app.css';
+  </script>
