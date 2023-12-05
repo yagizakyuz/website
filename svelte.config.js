005e128228cc9943e2ab4c7451835a036ff8a54f
Yagiz Akyuz <yagiz.akyuz@timewax.com>
2023-12-06 02:21:36 +0300

    Fixing the link.

--- svelte.config.js
+++ svelte.config.js
@@ -9,8 +9,8 @@ const config = {
 
     kit: {
         paths: {
-            base: process.env.NODE_ENV === 'production' ? '/website' : ''
-          },
+            base: '/website'
+        },
         adapter: adapter({
             pages: 'build',
             assets: 'build',
