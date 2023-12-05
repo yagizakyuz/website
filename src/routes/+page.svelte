b8ebd12b3cb782951dffce219d5f1e38a4508c26
Yagiz Akyuz <yagiz.akyuz@timewax.com>
2023-12-06 02:35:44 +0300

    Updated to SSR so we can serve nicely.

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,7 +1,8 @@
+<script>
+    import '../app.css';
+</script>
+
 <h1>Welcome to SvelteKit</h1>
 <p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
 <button class="btn btn-primary">Click me</button>
 <a href="/website/runna">rrrunnnaa</a>
-<script>
-    import '../app.css';
-  </script>
