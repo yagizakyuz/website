aba9e641ca8317823a88da98a1803a0243a12e6f
Yagiz Akyuz <yagiz.akyuz@timewax.com>
2023-12-06 01:27:32 +0300

    Test button

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,7 +1,7 @@
 <h1>Welcome to SvelteKit</h1>
 <p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
 <button class="btn btn-primary">Click me</button>
-
+<a href="/runna">rrrunnnaa</a>
 <script>
     import '../app.css';
   </script>
